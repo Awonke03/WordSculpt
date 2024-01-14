8ff174337d60342df96061049e9a47695a637b26
Awonke03 <nomandondo.awonke@outlook.com>
2024-01-15 01:23:00 +0200

    j

--- Play.Designer.cs
+++ Play.Designer.cs
@@ -31,18 +31,53 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Play));
             pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
+            txth1 = new TextBox();
+            txth6 = new TextBox();
+            txth5 = new TextBox();
+            txth4 = new TextBox();
+            txth3 = new TextBox();
+            txth2 = new TextBox();
+            label1 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            groupBox1 = new GroupBox();
+            rbnint = new RadioButton();
+            rbnhard = new RadioButton();
+            rbneasy = new RadioButton();
+            label14 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            lbl2 = new Label();
+            lbl3 = new Label();
+            lbl4 = new Label();
+            lbl5 = new Label();
+            lbl6 = new Label();
+            lbl1 = new Label();
+            menuStrip1 = new MenuStrip();
+            btnCheck = new Button();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(-21, -8);
+            pictureBox1.Location = new Point(-8, -14);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(884, 497);
+            pictureBox1.Size = new Size(884, 562);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // pictureBox2
             // 
@@ -53,24 +88,425 @@
             pictureBox2.TabIndex = 4;
             pictureBox2.TabStop = false;
             // 
+            // txth1
+            // 
+            txth1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            txth1.Location = new Point(94, 291);
+            txth1.Name = "txth1";
+            txth1.Size = new Size(180, 27);
+            txth1.TabIndex = 5;
+            // 
+            // txth6
+            // 
+            txth6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            txth6.Location = new Point(94, 460);
+            txth6.Name = "txth6";
+            txth6.Size = new Size(180, 27);
+            txth6.TabIndex = 9;
+            // 
+            // txth5
+            // 
+            txth5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            txth5.Location = new Point(94, 426);
+            txth5.Name = "txth5";
+            txth5.Size = new Size(180, 27);
+            txth5.TabIndex = 10;
+            // 
+            // txth4
+            // 
+            txth4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            txth4.Location = new Point(94, 390);
+            txth4.Name = "txth4";
+            txth4.Size = new Size(180, 27);
+            txth4.TabIndex = 11;
+            // 
+            // txth3
+            // 
+            txth3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            txth3.Location = new Point(94, 357);
+            txth3.Name = "txth3";
+            txth3.Size = new Size(180, 27);
+            txth3.TabIndex = 12;
+            // 
+            // txth2
+            // 
+            txth2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            txth2.Location = new Point(94, 324);
+            txth2.Name = "txth2";
+            txth2.Size = new Size(180, 27);
+            txth2.TabIndex = 13;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(55, 298);
+            label1.Name = "label1";
+            label1.Size = new Size(22, 20);
+            label1.TabIndex = 17;
+            label1.Text = "1:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(55, 463);
+            label6.Name = "label6";
+            label6.Size = new Size(22, 20);
+            label6.TabIndex = 22;
+            label6.Text = "6:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(55, 430);
+            label7.Name = "label7";
+            label7.Size = new Size(22, 20);
+            label7.TabIndex = 23;
+            label7.Text = "5:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(55, 397);
+            label8.Name = "label8";
+            label8.Size = new Size(22, 20);
+            label8.TabIndex = 24;
+            label8.Text = "4:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(55, 364);
+            label9.Name = "label9";
+            label9.Size = new Size(22, 20);
+            label9.TabIndex = 25;
+            label9.Text = "3:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label12.Location = new Point(55, 331);
+            label12.Name = "label12";
+            label12.Size = new Size(22, 20);
+            label12.TabIndex = 28;
+            label12.Text = "2:";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label13.Location = new Point(12, 265);
+            label13.Name = "label13";
+            label13.Size = new Size(207, 20);
+            label13.TabIndex = 29;
+            label13.Text = "Enter Correct Word Spelling:";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(rbnint);
+            groupBox1.Controls.Add(rbnhard);
+            groupBox1.Controls.Add(rbneasy);
+            groupBox1.Location = new Point(541, 410);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(324, 96);
+            groupBox1.TabIndex = 30;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Choose level :";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // rbnint
+            // 
+            rbnint.AutoSize = true;
+            rbnint.Location = new Point(125, 26);
+            rbnint.Name = "rbnint";
+            rbnint.Size = new Size(115, 24);
+            rbnint.TabIndex = 46;
+            rbnint.TabStop = true;
+            rbnint.Text = "Intermediate";
+            rbnint.UseVisualStyleBackColor = true;
+            rbnint.Click += rbnint_Click;
+            // 
+            // rbnhard
+            // 
+            rbnhard.AutoSize = true;
+            rbnhard.Location = new Point(65, 66);
+            rbnhard.Name = "rbnhard";
+            rbnhard.Size = new Size(63, 24);
+            rbnhard.TabIndex = 45;
+            rbnhard.TabStop = true;
+            rbnhard.Text = "Hard";
+            rbnhard.UseVisualStyleBackColor = true;
+            rbnhard.CheckedChanged += rbnhard_CheckedChanged;
+            rbnhard.Click += rbnhard_Click;
+            // 
+            // rbneasy
+            // 
+            rbneasy.AutoSize = true;
+            rbneasy.Location = new Point(18, 26);
+            rbneasy.Name = "rbneasy";
+            rbneasy.Size = new Size(59, 24);
+            rbneasy.TabIndex = 44;
+            rbneasy.TabStop = true;
+            rbneasy.Text = "Easy";
+            rbneasy.UseVisualStyleBackColor = true;
+            rbneasy.CheckedChanged += rbneasy_CheckedChanged;
+            rbneasy.Click += rbneasy_Click;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label14.Location = new Point(280, 265);
+            label14.Name = "label14";
+            label14.Size = new Size(121, 20);
+            label14.TabIndex = 31;
+            label14.Text = "Hints To Words:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(299, 327);
+            label2.Name = "label2";
+            label2.Size = new Size(22, 20);
+            label2.TabIndex = 37;
+            label2.Text = "2:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(299, 360);
+            label3.Name = "label3";
+            label3.Size = new Size(22, 20);
+            label3.TabIndex = 36;
+            label3.Text = "3:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(464, 298);
+            label4.Name = "label4";
+            label4.Size = new Size(22, 20);
+            label4.TabIndex = 35;
+            label4.Text = "4:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(464, 331);
+            label5.Name = "label5";
+            label5.Size = new Size(22, 20);
+            label5.TabIndex = 34;
+            label5.Text = "5:";
+            label5.Click += label5_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.Location = new Point(464, 364);
+            label10.Name = "label10";
+            label10.Size = new Size(22, 20);
+            label10.TabIndex = 33;
+            label10.Text = "6:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label11.Location = new Point(299, 294);
+            label11.Name = "label11";
+            label11.Size = new Size(22, 20);
+            label11.TabIndex = 32;
+            label11.Text = "1:";
+            // 
+            // lbl2
+            // 
+            lbl2.AutoSize = true;
+            lbl2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl2.Location = new Point(353, 327);
+            lbl2.Name = "lbl2";
+            lbl2.Size = new Size(22, 20);
+            lbl2.TabIndex = 43;
+            lbl2.Text = "2:";
+            // 
+            // lbl3
+            // 
+            lbl3.AutoSize = true;
+            lbl3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl3.Location = new Point(353, 360);
+            lbl3.Name = "lbl3";
+            lbl3.Size = new Size(22, 20);
+            lbl3.TabIndex = 42;
+            lbl3.Text = "3:";
+            lbl3.Click += label16_Click;
+            // 
+            // lbl4
+            // 
+            lbl4.AutoSize = true;
+            lbl4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl4.Location = new Point(492, 298);
+            lbl4.Name = "lbl4";
+            lbl4.Size = new Size(22, 20);
+            lbl4.TabIndex = 41;
+            lbl4.Text = "4:";
+            // 
+            // lbl5
+            // 
+            lbl5.AutoSize = true;
+            lbl5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl5.Location = new Point(492, 331);
+            lbl5.Name = "lbl5";
+            lbl5.Size = new Size(22, 20);
+            lbl5.TabIndex = 40;
+            lbl5.Text = "5:";
+            // 
+            // lbl6
+            // 
+            lbl6.AutoSize = true;
+            lbl6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl6.Location = new Point(492, 364);
+            lbl6.Name = "lbl6";
+            lbl6.Size = new Size(22, 20);
+            lbl6.TabIndex = 39;
+            lbl6.Text = "6:";
+            lbl6.Click += label19_Click;
+            // 
+            // lbl1
+            // 
+            lbl1.AutoSize = true;
+            lbl1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl1.Location = new Point(353, 298);
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(22, 20);
+            lbl1.TabIndex = 38;
+            lbl1.Text = "1:";
+            lbl1.Click += label20_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(877, 24);
+            menuStrip1.TabIndex = 44;
+            menuStrip1.Text = "menuStrip1";
+            menuStrip1.ItemClicked += menuStrip1_ItemClicked;
+            // 
+            // btnCheck
+            // 
+            btnCheck.Enabled = false;
+            btnCheck.Location = new Point(323, 477);
+            btnCheck.Name = "btnCheck";
+            btnCheck.Size = new Size(113, 29);
+            btnCheck.TabIndex = 45;
+            btnCheck.Text = "Check Words";
+            btnCheck.UseVisualStyleBackColor = true;
+            btnCheck.Click += btnCheck_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(701, 49);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 29);
+            button1.TabIndex = 46;
+            button1.Text = "Quit";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // Play
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(805, 487);
+            ClientSize = new Size(877, 551);
+            Controls.Add(button1);
+            Controls.Add(btnCheck);
+            Controls.Add(lbl2);
+            Controls.Add(lbl3);
+            Controls.Add(lbl4);
+            Controls.Add(lbl5);
+            Controls.Add(lbl6);
+            Controls.Add(lbl1);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            Controls.Add(label10);
+            Controls.Add(label11);
+            Controls.Add(label14);
+            Controls.Add(groupBox1);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label1);
+            Controls.Add(txth2);
+            Controls.Add(txth3);
+            Controls.Add(txth4);
+            Controls.Add(txth5);
+            Controls.Add(txth6);
+            Controls.Add(txth1);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Play";
             Text = "Play";
             Load += Play_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
+        private TextBox txth1;
+        private TextBox txth6;
+        private TextBox txth5;
+        private TextBox txth4;
+        private TextBox txth3;
+        private TextBox txth2;
+        private Label label1;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label12;
+        private Label label13;
+        private GroupBox groupBox1;
+        private Label label14;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label10;
+        private Label label11;
+        private Label lbl2;
+        private Label lbl3;
+        private Label lbl4;
+        private Label lbl5;
+        private Label lbl6;
+        private Label lbl1;
+        private RadioButton rbnint;
+        private RadioButton rbnhard;
+        private RadioButton rbneasy;
+        private MenuStrip menuStrip1;
+        private Button btnCheck;
+        private Button button1;
     }
 }
--- Play.cs
+++ Play.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,10 +17,276 @@ namespace WordSculpt
         {
             InitializeComponent();
         }
+        string[] words;
+        string[] play = new string[6];
+        string[] letter = new string[6];
 
+
+        public void letters()
+        {
+            string l = "";
+
+            char[] alpha = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            int u = 0;
+
+            foreach (string w in play)
+            {
+                l = "";
+
+                foreach (char c in alpha)
+                {
+                    if (w.Contains(c))
+                        l = l + c;
+
+
+                }
+
+                letter[u] = l;
+                u++;
+
+            }
+        }
+        Random random = new Random();
+        public int word_length;
+        int maxAttempts = 30000; // or any other reasonable value
+        int attempts = 0;
+        public void generatewords()
+        {
+            int num = 0;
+            string s = "";
+            for (int i = 0; i < 6; i++)
+            {
+                attempts = 0;
+
+                do
+                {
+                    num = random.Next(len);
+                    s = words[num];
+                    attempts++;
+                    if (attempts >= maxAttempts)
+                    {
+                        //MessageBox.Show($"{attempts}");
+                        break;
+                    }
+
+
+                } while (s.Length > word_length);
+                //MessageBox.Show($"{num}");
+                play[i] = words[num];
+            }
+
+            letters();
+
+
+        }
+        public int len = 0;
         private void Play_Load(object sender, EventArgs e)
         {
+            int i = 0; int j = 0;
+            if (File.Exists("Words.txt"))
+            {
+                words = File.ReadAllLines("Words.txt");
+                foreach (string word in words)
+                {
+                    string cleanLine = Regex.Replace(word, "[^a-zA-Z]", " ");
+                    if (cleanLine.Length > 1 && cleanLine != "")
+                    {
+                        words[i] = cleanLine;
+                        if (cleanLine.Length > j)
+                            j = cleanLine.Length;
+                        i++;
 
+                    }
+
+                }
+                // MessageBox.Show($"{j}");
+                len = i;
+
+
+
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void rbneasy_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbneasy_Click(object sender, EventArgs e)
+        {
+            generatewords();
+            word_length = 5;
+            rbneasy.Checked = true;
+            rbnhard.Checked = false;
+            rbnint.Checked = false;
+            btnCheck.Enabled = true;
+            lbl1.Text = letter[0];
+            lbl2.Text = letter[1];
+            lbl3.Text = letter[2];
+            lbl4.Text = letter[3];
+            lbl5.Text = letter[4];
+            lbl6.Text = letter[5];
+        }
+
+        private void rbnint_Click(object sender, EventArgs e)
+        {
+            generatewords();
+            word_length = 10;
+            rbnint.Checked = true;
+            rbnhard.Checked = false;
+            rbneasy.Checked = false;
+            btnCheck.Enabled = true;
+            lbl1.Text = letter[0];
+            lbl2.Text = letter[1];
+            lbl3.Text = letter[2];
+            lbl4.Text = letter[3];
+            lbl5.Text = letter[4];
+            lbl6.Text = letter[5];
+        }
+
+        private void rbnhard_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbnhard_Click(object sender, EventArgs e)
+        {
+            generatewords();
+            word_length = 18;
+            rbnhard.Checked = true;
+            rbneasy.Checked = false;
+            rbnint.Checked = false;
+            btnCheck.Enabled = true;
+            lbl1.Text = letter[0];
+            lbl2.Text = letter[1];
+            lbl3.Text = letter[2];
+            lbl4.Text = letter[3];
+            lbl5.Text = letter[4];
+            lbl6.Text = letter[5];
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (txth1.Text != letter[0])
+                MessageBox.Show($"word 1 is not correct");
+
+            else if (txth2.Text != letter[1])
+                MessageBox.Show($"word 2 is not correct");
+            else if (txth3.Text != letter[2])
+                MessageBox.Show($"word 3 is not correct");
+            else if (txth4.Text != letter[3])
+                MessageBox.Show($"word 4 is not correct");
+            else if (txth5.Text != letter[4])
+                MessageBox.Show($"word 5 is not correct");
+            else if (txth6.Text != letter[5])
+                MessageBox.Show($"word 1 is not correct");
+            else
+            {
+                MessageBox.Show($"Congradulations!!!\n You have Won the game", "Win");
+
+
+
+                if (rbnhard.Checked == true)
+                {
+                    word_length = 18;
+                    generatewords();
+                    lbl1.Text = letter[0];
+                    lbl2.Text = letter[1];
+                    lbl3.Text = letter[2];
+                    lbl4.Text = letter[3];
+                    lbl5.Text = letter[4];
+                    lbl6.Text = letter[5];
+                    rbneasy.Checked = false;
+                    rbnint.Checked = false;
+                    btnCheck.Enabled = true;
+
+                }
+
+                else if (rbneasy.Checked == false)
+                {
+                    word_length = 6;
+                    generatewords();
+                    lbl1.Text = letter[0];
+                    lbl2.Text = letter[1];
+                    lbl3.Text = letter[2];
+                    lbl4.Text = letter[3];
+                    lbl5.Text = letter[4];
+                    lbl6.Text = letter[5];
+                    rbneasy.Checked = false;
+                    rbnint.Checked = false;
+                    btnCheck.Enabled = true;
+
+                }
+
+                else if (rbnint.Checked = false)
+                {
+                    word_length = 11;
+                    generatewords();
+                    lbl1.Text = letter[0];
+                    lbl2.Text = letter[1];
+                    lbl3.Text = letter[2];
+                    lbl4.Text = letter[3];
+                    lbl5.Text = letter[4];
+                    lbl6.Text = letter[5];
+                    rbneasy.Checked = false;
+                    rbnint.Checked = false;
+                    btnCheck.Enabled = true;
+
+                }
+                else
+                {
+                    btnCheck.Enabled = false;
+                    MessageBox.Show("Select A level of difficulty");
+                }
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
